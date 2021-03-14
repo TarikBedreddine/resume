27c6fefc11d628f5ba545b7daeb97e17428ed984
TarikBedreddine <tarik.bedreddine@yahoo.com>
2021-03-14 16:19:22 +0100

    feat: set the background images with the appropriate style

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--TITLES & DESCRIPTION-->
+    <title>Titre</title>
+    <meta name="description" content="Mon cv"/>
+    <!--STYLESHEET-CSS-->
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+    
+
+<body>
+<main>
+    <div class="outer-wrapper">
+        <div class="wrapper">
+            <div class="slide one">
+                Slide one
+            </div>
+            <div class="slide two">
+                Slide Two
+            </div>
+            <div class="slide three">
+                Slide Three
+            </div>
+            <div class="slide four">
+                Slide Four
+            </div>
+            <div class="slide five">
+                Slide Five
+            </div>
+            <div class="slide six">
+                Slide Six
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
