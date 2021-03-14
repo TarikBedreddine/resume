09794108b0f4d9d855d0734987869763ec68a480
TarikBedreddine <tarik.bedreddine@yahoo.com>
2021-03-14 18:39:27 +0100

    feat: first slide done

--- index.php
+++ index.php
@@ -8,15 +8,25 @@
     <meta name="description" content="Mon cv"/>
     <!--STYLESHEET-CSS-->
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,700;0,800;0,900;1,700&display=swap" rel="stylesheet">
 </head>
-    
+
 
 <body>
 <main>
     <div class="outer-wrapper">
         <div class="wrapper">
-            <div class="slide one">
-                Slide one
+            <div class="one slide">
+                <div class="content-one">
+                    <h1>Tarik, <br> Développeur Web</h1>
+                    <div class="flex-img_paragraph">
+                        <img src="images/slide-1/me.jpg" alt="Ma photo">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it.</p>
+                    </div>
+                </div>
             </div>
             <div class="slide two">
                 Slide Two
