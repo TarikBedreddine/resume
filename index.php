35ae3fa16f1ed3503b686b75af42ddc712d3dd9b
TarikBedreddine <tarik.bedreddine@yahoo.com>
2021-03-15 21:22:36 +0100

    feat: slide two done

--- index.php
+++ index.php
@@ -17,20 +17,40 @@
 <main>
     <div class="outer-wrapper">
         <div class="wrapper">
-            <div class="one slide">
-                <div class="content-one">
-                    <h1>Tarik, <br> Développeur Web</h1>
-                    <div class="flex-img_paragraph">
-                        <img src="images/slide-1/me.jpg" alt="Ma photo">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it.</p>
+            <section class="slide one">
+                    <article class="content-one">
+                            <h1>Tarik, <br> Développeur Web</h1>
+                            <div class="flex-img_paragraph">
+                                <img src="images/slide-1/me.jpg" alt="Ma photo">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    when an unknown printer took a galley of type and scrambled it.
+                                </p>
+                            </div>
+                    </article>
+            </section>
+            <section class="slide two">
+                <article class="content-two">
+                    <h2>Développement</h2>
+                    <div class="rectangle-background">
+                        <div class="programming-languages">
+                            <img src="images/slide-2/html.png" alt="html logo">
+                            <img src="images/slide-2/css.png" alt="css logo">
+                            <img src="images/slide-2/php.png" alt="php logo">
+                            <img src="images/slide-2/js.png" alt="js logo">
+                            <img src="images/slide-2/java.png" alt="java logo">
+                        </div>
+                        <div class="tools-container">
+                            <div class="tools">
+                                <img src="images/slide-2/vs-code.png" alt="vs-code logo">
+                                <img src="images/slide-2/phpstorm.png" alt="php storm logo">
+                                <img src="images/slide-2/git.png" alt="git logo">
+                                <img src="images/slide-2/trello.png" alt="trello logo">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="slide two">
-                Slide Two
-            </div>
+                </article>
+            </section>
             <div class="slide three">
                 Slide Three
             </div>
