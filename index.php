9e4468a4c929edc15e12809b15792d2b958f076f
TarikBedreddine <tarik.bedreddine@yahoo.com>
2021-03-17 19:56:16 +0100

    fix/feat: the positioning of content sections / slide three done

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <!--TITLES & DESCRIPTION-->
     <title>Titre</title>
     <meta name="description" content="Mon cv"/>
-    <!--STYLESHEET-CSS-->
+    <!--STYLESHEET-CSS / FONTS-->
     <link rel="stylesheet" href="styles/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,700;0,800;0,900;1,700&display=swap" rel="stylesheet">
@@ -15,10 +15,17 @@
 
 <body>
 <main>
+
+
+<!--    GLOBAL WRAPPER & CONTAINER TO GET HORIZONTAL POSITION -->
+
     <div class="outer-wrapper">
         <div class="wrapper">
-            <section class="slide one">
-                    <article class="content-one">
+
+<!--            SLIDE 1 ABOUT ME -->
+
+            <div class="slide one">
+                    <section class="content-one">
                             <h1>Tarik, <br> Développeur Web</h1>
                             <div class="flex-img_paragraph">
                                 <img src="images/slide-1/me.jpg" alt="Ma photo">
@@ -27,33 +34,54 @@
                                     when an unknown printer took a galley of type and scrambled it.
                                 </p>
                             </div>
-                    </article>
-            </section>
-            <section class="slide two">
-                <article class="content-two">
-                    <h2>Développement</h2>
-                    <div class="rectangle-background">
-                        <div class="programming-languages">
-                            <img src="images/slide-2/html.png" alt="html logo">
-                            <img src="images/slide-2/css.png" alt="css logo">
-                            <img src="images/slide-2/php.png" alt="php logo">
-                            <img src="images/slide-2/js.png" alt="js logo">
-                            <img src="images/slide-2/java.png" alt="java logo">
-                        </div>
-                        <div class="tools-container">
-                            <div class="tools">
-                                <img src="images/slide-2/vs-code.png" alt="vs-code logo">
-                                <img src="images/slide-2/phpstorm.png" alt="php storm logo">
-                                <img src="images/slide-2/git.png" alt="git logo">
-                                <img src="images/slide-2/trello.png" alt="trello logo">
-                            </div>
-                        </div>
-                    </div>
-                </article>
-            </section>
-            <div class="slide three">
-                Slide Three
+                    </section>
             </div>
+
+<!--            SLIDE 2 MAJOR COMPETENCES -->
+
+            <div class="slide two">
+                <section class="content-two">
+                        <h2>Développement</h2>
+                        <div class="rectangle-background">
+                            <article class="programming-languages">
+                                <img src="images/slide-2/html.png" alt="html logo">
+                                <img src="images/slide-2/css.png" alt="css logo">
+                                <img src="images/slide-2/php.png" alt="php logo">
+                                <img src="images/slide-2/js.png" alt="js logo">
+                                <img src="images/slide-2/java.png" alt="java logo">
+                            </article>
+                            <article class="tools-container">
+                                <div class="tools">
+                                    <img src="images/slide-2/vs-code.png" alt="vs-code logo">
+                                    <img src="images/slide-2/phpstorm.png" alt="php storm logo">
+                                    <img src="images/slide-2/git.png" alt="git logo">
+                                    <img src="images/slide-2/trello.png" alt="trello logo">
+                                </div>
+                            </article>
+                        </div>
+                </section>
+            </div>
+
+<!--            SLIDE 2BIS COMPETENCES -->
+
+            <div class="slide three">
+                <section class="content-three">
+                    <div class="rectangle-background-1">
+                        <article class="content-three-design">
+                            <h3>Graphisme</h3>
+                            <img src="images/slide-3/design.png" alt="Graphisme">
+                        </article>
+                    </div>
+                    <div class="rectangle-background-2">
+                        <article class="content-three-languages">
+                            <h3>Informatique & Langues</h3>
+                            <img src="images/slide-3/computer.png" alt="Ordinateur">
+                            <img src="images/slide-3/language.png" alt="Langues">
+                        </article>
+                    </div>
+                </section>
+            </div>
+
             <div class="slide four">
                 Slide Four
             </div>
